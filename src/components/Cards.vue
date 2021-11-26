3006fa765daaa8db062f0adc250bb37090ae35eb
LucaVerth <luca.verthuy@gmail.com>
2021-11-26 18:17:41 +0100

    Work done, still some minor bugs to fix

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>CARDS</div>
+
+  <div class="col-sm-3 col-lg-2 album-card">
+    <div class="album-cover">
+      <img :src="album.poster" :alt="album.title">
+    </div>
+    <h5>{{album.title}}</h5>
+    <span>{{album.author}}</span>
+    <span>{{album.year}}</span>
+  </div>
+ 
 </template>
 
 <script>
 export default {
-
+  name: 'Cards',
+  props:{
+    album: Object
+  }
 }
 </script>
 
 <style lang="scss">
-
+@import '../assets/style/vars.scss';
+@import '../assets/style/mixins.scss';
+.album-card{
+  display: flex;
+  flex-direction:column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: $primary-color;
+  margin: 15px;
+  .album-cover{
+    //min-width: 40px;
+    margin: 10px;
+  }
+  h5{
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 1rem;
+  }
+  span{
+    color: grey;
+  }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,49 @@
 <template>
   <main>
-    MAIN
-    <div class="album-container">
-      <div>
-        <Cards/>
+    <div class="album-container py-5">
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <Cards
+            v-for="(album, index) in albums"
+            :key="index"
+            :album="album"
+          />
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 import Cards from "./Cards.vue";
 
 export default {
   components:{
     Cards,
+  },
+  data(){
+    return{
+      albums: [],
+      apiData: 'https://flynn.boolean.careers/exercises/api/array/music'
+
+    }
+  },
+  methods:{
+    getData(){
+      axios.get(this.apiData)
+        .then(response =>{
+          let data = response.data
+          console.log(data);
+          this.albums = data.response;
+        })
+        .catch(error =>{
+          console.error(error);
+        })
+    }
+  },
+  mounted(){
+    this.getData();
   }
 }
 </script>
@@ -24,7 +53,13 @@ export default {
 @import '../assets/style/mixins.scss';
 
 main{
-  height: calc(100vh - 70px);
+  min-height: calc(100vh - 70px);
   background-color: $secondary-color;
+  .album-container{
+    display: flex;
+    justify-content: center;
+    width: 70%;
+    margin: 0 auto;
+  }
 }
 </style>
